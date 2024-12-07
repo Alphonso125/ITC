12dbf2d03d32b761a4e1cf6ba954242443b1319b
dar <darlanohan@addu.edu.ph>
2024-12-07 23:01:12 +0800

    conversionFormulas.js

--- conversionFormulas.js
+++ conversionFormulas.js
@@ -3,7 +3,7 @@ function cToF(){
     
     c = document.getElementById("value").value*1;
 
-    let f = ((c * (9/5))) + 32;
+    let f = (((c * (9/5))) + 32).toFixed(2);
 
     document.getElementById("converted").value = f +' F'+'°'; 
 
@@ -14,7 +14,7 @@ function fToC(){
 
     f = document.getElementById("value").value*1;
 
-    let c = (f - 32) * (5 / 9);
+    let c = ((f - 32) * (5 / 9)).toFixed(2);
 
     document.getElementById("converted").value = c +' C'+'°'; 
 
@@ -24,7 +24,7 @@ function mToF(){
 
     m = document.getElementById("value").value*1;
 
-    let ft = m * 3.28084;
+    let ft = (m * 3.28084).toFixed(2);
 
     document.getElementById("converted").value = ft + ' m';
 }
@@ -33,7 +33,7 @@ function fToM(){
 
     f = document.getElementById("value").value*1;
 
-    let m = f * 0.3048;
+    let m = (f * 0.3048).toFixed(2);
 
     document.getElementById("converted").value = m + ' ft';
 }
